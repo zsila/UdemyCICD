64709f2758f302a2dc46fa50e2314e115d69c293
Sıla <zisansila@gmail.com>
2023-09-29 14:46:29 +0200

    Remove first Greeting text

--- app/src/main/java/com/example/udemycicd/MainActivity.kt
+++ app/src/main/java/com/example/udemycicd/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Hello CI/CD World!")
+                    Greeting("")
                     Greeting("Feature-1")
                 }
 
